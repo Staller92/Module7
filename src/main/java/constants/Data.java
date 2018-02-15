89d3306fc305c57176a3ea5ab7ef741d50eda2ee
Dzmitry Likhtarovich <Dzmitry_Likhtarovich@epam.com>
2018-02-15 16:56:19 +0300

    update

---
+++ src/main/java/constants/Data.java
@@ -0,0 +1,22 @@
+package constants;
+
+/**
+ * Created by Dzmitry_Likhtarovich on 2/12/2018.
+ */
+public class Data {
+
+    public static final String URL = "https://www.yandex.by/";
+    public static final String LOGIN = "dzmitry.likhtarovich";
+    public static final String PASSWORD = "deathnote";
+    public static final String ADDRESS = "dimalix92@gmail.com";
+    public static final String THEME = "about something";
+    public static final String CONTENT = "about something in detail";
+    public static final String SCREENSHOTS_NAME_TPL = "screenshots/scr";
+    public static final String CHROME_DRIVER ="webdriver.chrome.driver";
+    public static final String PATH_TO_CHROME_DRIVER ="/driver/chromedriver.exe";
+    public static final int TIME = 20;
+    public static final int IMPLICITLY_WAIT = 10;
+    public static final int PAGE_LOAD_TIME_OUT = 15;
+
+}
+
