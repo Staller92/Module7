a10b82c0f49a7624d440a107449a8d657d1cd9c2
Dzmitry Likhtarovich <Dzmitry_Likhtarovich@epam.com>
2018-02-15 15:44:22 +0300

    update

---
+++ src/test/java/TestShiftDraftToBin.java
@@ -0,0 +1,46 @@
+import constants.Data;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.DraftPage;
+import pages.LoginPage;
+import pages.NewLatterPage;
+import util.WebDriverSingleton;
+
+/**
+ * Created by Dzmitry_Likhtarovich on 2/14/2018.
+ */
+public class TestShiftDraftToBin {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private NewLatterPage newLatterPage;
+    private DraftPage draftPage;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = WebDriverSingleton.getWebDriverInstance();
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+
+        WebDriverSingleton.getWebDriverInstance().quit();
+
+    }
+
+    @Test
+    public void testShiftLetterToBin() {
+        driver.get(Data.URL);
+        loginPage = new LoginPage();
+        newLatterPage = loginPage.loginAs(Data.LOGIN, Data.PASSWORD).createNewLetter();
+        newLatterPage.fillDestinationField(Data.ADDRESS);
+        draftPage = newLatterPage.fillThemeField(Data.THEME).openDrafts();
+        Assert.assertTrue(draftPage.isLetterExistInDrafts(Data.ADDRESS));
+        draftPage.shiftLetterToBin(Data.ADDRESS);
+        Assert.assertFalse(draftPage.isLetterExistInDrafts(Data.ADDRESS));
+    }
+}
